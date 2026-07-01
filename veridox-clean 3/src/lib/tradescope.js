df79465ae9a2ec54e4a9f7165f1bdc7535c2b767
SavvidesP <panayiotis09082000@gmail.com>
2026-07-01 12:16:54 +0300

    Update tradescope.js

--- veridox-clean 3/src/lib/tradescope.js
+++ veridox-clean 3/src/lib/tradescope.js
@@ -5,7 +5,7 @@ import { createClient } from '@supabase/supabase-js';
 // later, to apply trading adjustments. Veridox users do NOT authenticate here,
 // so session persistence is disabled to avoid clashing with the Veridox auth client.
 const tradescopeUrl = 'https://atqucerzdqzchdgylmfo.supabase.co';
-const tradescopeAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF0cXVjZXJ6ZHF6Y2hkZ3lsbWZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIzOTMzMjYsImV4cCI6MjA5Nzk2OTMyNn0.rI3v-lVdsSX2VQUyVxYE4A4EsDsYlexTz5llVsqbx7I';
+const tradescopeAnonKey = 'sb_publishable_znSIOy8SHNmOb8pfeQtIRg_NY5wPGxK';
 
 export const tradescope = createClient(tradescopeUrl, tradescopeAnonKey, {
   auth: { persistSession: false, autoRefreshToken: false },
