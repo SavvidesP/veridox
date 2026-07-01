7c9b3746ab0673a394fbaa5ce8eedfd30d2a8c72
SavvidesP <panayiotis09082000@gmail.com>
2026-07-02 00:34:27 +0300

    Update App.jsx

--- veridox-clean 3/src/App.jsx
+++ veridox-clean 3/src/App.jsx
@@ -8,13 +8,10 @@ import ResetPassword from './pages/ResetPassword';
 import Dashboard from './pages/Dashboard';
 import ClientList from './pages/ClientList';
 import ClientProfile from './pages/ClientProfile';
-import KYCPipeline from './pages/KYCPipeline';
 import AddClient from './pages/AddClient';
 import Settings from './pages/Settings';
 import Transactions from './pages/Transactions';
 import Routing from './pages/Routing';
-import Disputes from './pages/Disputes';
-import FraudRules from './pages/FraudRules';
 import Analytics from './pages/Analytics';
 import Cascading from './pages/Cascading';
 import TransactionDetail from './pages/TransactionDetail';
@@ -23,10 +20,7 @@ import TradingAccounts from './pages/TradingAccounts';
 import TradingAccountDetail from './pages/TradingAccountDetail';
 import TeamMemberDetail from './pages/TeamMemberDetail';
 import BonusManagement from './pages/BonusManagement';
-import IBAffiliate from './pages/IBAffiliate';
-import FinancialReports from './pages/FinancialReports';
 import WithdrawalApprovals from './pages/WithdrawalApprovals';
-import DocumentCenter from './pages/DocumentCenter';
 import CommunicationCenter from './pages/CommunicationCenter';
 import SalesCRM from './pages/SalesCRM';
 import ConvertedClients from './pages/ConvertedClients';
@@ -79,25 +73,19 @@ function App() {
         <Route path="/dashboard" element={<ProtectedRoute><Layout><Dashboard /></Layout></ProtectedRoute>} />
         <Route path="/clients" element={<ProtectedRoute><Layout><ClientList /></Layout></ProtectedRoute>} />
         <Route path="/clients/:id" element={<ProtectedRoute><Layout><ClientProfile /></Layout></ProtectedRoute>} />
-        <Route path="/pipeline" element={<ProtectedRoute><Layout><KYCPipeline /></Layout></ProtectedRoute>} />
         <Route path="/add-client" element={<AdminRoute><Layout><AddClient /></Layout></AdminRoute>} />
         <Route path="/settings" element={<AdminRoute><Layout><Settings /></Layout></AdminRoute>} />
         <Route path="/team/:id" element={<AdminRoute><Layout><TeamMemberDetail /></Layout></AdminRoute>} />
         <Route path="/transactions" element={<ProtectedRoute><Layout><Transactions /></Layout></ProtectedRoute>} />
         <Route path="/transactions/:id" element={<ProtectedRoute><Layout><TransactionDetail /></Layout></ProtectedRoute>} />
         <Route path="/routing" element={<AdminRoute><Layout><Routing /></Layout></AdminRoute>} />
-        <Route path="/disputes" element={<AdminRoute><Layout><Disputes /></Layout></AdminRoute>} />
-        <Route path="/fraud-rules" element={<AdminRoute><Layout><FraudRules /></Layout></AdminRoute>} />
         <Route path="/analytics" element={<ProtectedRoute><Layout><Analytics /></Layout></ProtectedRoute>} />
         <Route path="/cascading" element={<AdminRoute><Layout><Cascading /></Layout></AdminRoute>} />
         <Route path="/integrations" element={<AdminRoute><Layout><Integrations /></Layout></AdminRoute>} />
         <Route path="/trading-accounts" element={<ProtectedRoute><Layout><TradingAccounts /></Layout></ProtectedRoute>} />
         <Route path="/trading-accounts/:id" element={<ProtectedRoute><Layout><TradingAccountDetail /></Layout></ProtectedRoute>} />
         <Route path="/bonus-management" element={<ProtectedRoute><Layout><BonusManagement /></Layout></ProtectedRoute>} />
-        <Route path="/ib-affiliate" element={<AdminRoute><Layout><IBAffiliate /></Layout></AdminRoute>} />
-        <Route path="/financial-reports" element={<AdminRoute><Layout><FinancialReports /></Layout></AdminRoute>} />
         <Route path="/withdrawal-approvals" element={<ProtectedRoute><Layout><WithdrawalApprovals /></Layout></ProtectedRoute>} />
-        <Route path="/document-center" element={<ProtectedRoute><Layout><DocumentCenter /></Layout></ProtectedRoute>} />
         <Route path="/communication-center" element={<ProtectedRoute><Layout><CommunicationCenter /></Layout></ProtectedRoute>} />
         <Route path="/sales-crm" element={<ProtectedRoute><Layout><SalesCRM /></Layout></ProtectedRoute>} />
         <Route path="/converted-clients" element={<ProtectedRoute><Layout><ConvertedClients /></Layout></ProtectedRoute>} />
